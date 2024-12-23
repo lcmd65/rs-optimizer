e282fd3f870c859f653aa6324a27f48c1bc9806a
Dat Le <dat@workforceoptimizer.com>
2024-12-23 14:11:55 +0700

    [ARD-2614]

---
+++ src/auth-service/data.rs
@@ -0,0 +1,10 @@
+use crate::user::User;
+
+pub static User: Lazy<Mutex<HashMap<u32, User>>> = Lazy::new(|| Mutex::new(None));
+
+fn initialize_from_json(json: &str) {
+    let user_data: UserData = serde_json::from_str(json).expect("Invalid JSON");
+
+    let mut global_data = GLOBAL_USER.lock().unwrap();
+    *global_data = Some(user_data);
+}
---
+++ src/auth-service/user.rs
@@ -0,0 +1,4 @@
+pub struct User{
+    pub (crate) username: String,
+    pub (crate) password: String
+}
