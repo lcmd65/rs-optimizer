4456495c8d0266f40774c8a5691f17200fa2194c
Dat Le <dat@workforceoptimizer.com>
2024-12-23 14:25:39 +0700

    [ARD-2614]

--- src/auth_service/data.rs
+++ src/auth_service/data.rs
@@ -1,11 +1,12 @@
 use once_cell::sync::Lazy;
 use std::sync::Mutex;
 use crate::User;
+use std::collections::{HashMap};
 
-pub static User: Lazy<Mutex<HashMap<u32, User>>> = Lazy::new(|| Mutex::new(None));
+pub static GLOBAL_USER: Lazy<Mutex<HashMap<u32, User>>> = Lazy::new(|| Mutex::new(None));
 
 fn initialize_from_json(json: &str) {
-    let user_data: UserData = serde_json::from_str(json).expect("Invalid JSON");
+    let user_data: User = serde_json::from_str(json).expect("Invalid JSON");
 
     let mut global_data = GLOBAL_USER.lock().unwrap();
     *global_data = Some(user_data);
