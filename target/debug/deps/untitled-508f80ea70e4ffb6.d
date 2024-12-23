3f521c5962cd9e3bfbd12c83e0f9bdb429674668
Le Chon Minh Dat <93373784+lcmd65@users.noreply.github.com>
2024-12-23 10:03:12 +0700

    Delete target directory

--- target/debug/deps/untitled-508f80ea70e4ffb6.d
+++
@@ -1,8 +0,0 @@
-/Users/lechonminhdat/Documents/rs-optimizer/target/debug/deps/untitled-508f80ea70e4ffb6: src/main.rs src/service/mod.rs src/service/macro.rs src/templates/mod.rs
-
-/Users/lechonminhdat/Documents/rs-optimizer/target/debug/deps/untitled-508f80ea70e4ffb6.d: src/main.rs src/service/mod.rs src/service/macro.rs src/templates/mod.rs
-
-src/main.rs:
-src/service/mod.rs:
-src/service/macro.rs:
-src/templates/mod.rs:
